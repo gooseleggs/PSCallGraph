9c99644df17fa8464a3e3e0bb290588d4769835c
Kelvin Smith <kelvin@thesmithcave.nz>
2025-01-19 11:07:18 +1300

    Add better code documentation and function description

--- pscallgraph.ps1
+++ pscallgraph.ps1
@@ -30,19 +30,80 @@ $callStack = New-Object System.Collections.Stack
 $callStack.push('main')
 
 function Get-ResolvePath {
+<#
+	.SYNOPSIS
+		Creates a path relative to the source if a fixed path is not given for the destination
+
+	.DESCRIPTION
+		This function is used to return the path of a file.  If a relative is provided for the destination, then
+		this is worked out using the source path as the base path.  This is used to calculate paths that are relative
+		to the source directory.  If the destination path is fixed (fully qualified), then we ignore the source path
+		and just return the destination value
+
+	.PARAMETER sourcePath
+		The base path that is used for any relative path of destination
+
+	.PARAMETER destPath
+		The desired destination path, either relative to sourcePath or fully qualified
+
+	.EXAMPLE
+		Get-Resolvepath -sourcePath "C:\temp" -destPath 'output\logs.txt'
+
+	.INPUTS
+		Both parameters are Strings
+
+	.OUTPUTS
+		String
+#>
+	[CmdletBinding()]
 	PARAM(
-		$sourcePath,
-		$destPath
+		[Parameter(Mandatory)]
+		[string] $sourcePath,
+		[Parameter(Mandatory)]
+		[string] $destPath
 	)
 	
+	# If we are provided a fixed path, then the source path does not matter
 	if ([System.IO.Path]::IsPathRooted($destPath)) {
 		return $destPath
 	}
+
+	# .. Otherwise return the source joined with the destination path
 	return Join-Path -Path (Split-Path -path $sourcePath) -ChildPath $destPath
 }
 
 function disposeFile {
+<#
+	.SYNOPSIS
+		Reads through a PowerShell script building a call graph
+
+	.DESCRIPTION
+		This function reads through a powershell script to find all functions defined within the script and
+		all functions that call those defined functions.  If the script dot sources another script this is
+		read in as well and processed.  This function is recursive.
+
+	.PARAMETER file
+		The path and file name of the file to be read.  It is assumed to be a .ps1 file
+
+	.EXAMPLE
+		disposeFile -file 'C:\temp\test.ps1'
+
+	.INPUTS
+		String
+
+	.OUTPUTS
+		Does not return anything, however, it expands the global variable $functions (which must be defined
+		outside of the function, such as
+
+		# Initialize a hashtable to store functions and their calls
+		$functions = @{}
+
+		# As there is no function called main, define it exclusively
+		$functions['main'] = @()
+#>
+	[CmdletBinding()]
 	param (
+		[Parameter(Mandatory)]
 		[string] $file
 )
 	write-output "Reading file $file"
@@ -53,7 +114,7 @@ function disposeFile {
 		return	
 	}
 
-	# Read in the file
+	# Convert the file into tokens for processing
 	$Tokens = [System.Management.Automation.PSParser]::Tokenize($scriptContent, [Ref] $Null) | Where-Object { $_.Type -ne 'Comment' }
 
 	# Parse 1 - Find all the functions
@@ -61,7 +122,7 @@ function disposeFile {
 
 	write-Output "===== Parse 1 - Find functions ====="
 
-	# This tracks if we have found a function keyword
+	# This tracks if we have found a function keyword as we need the next token that defines the function name
 	$foundFunction = $false
 	foreach ($token in $Tokens)
 	{
@@ -72,6 +133,7 @@ function disposeFile {
 		}
 		if ($foundFunction) {
 			$foundFunction = $false
+			# As function names are case insenstive, we convert to lower case as mermaid is case sensitive
 			$functionName = ($token.Content).ToLower()
 			# Add it to the array
 			$functions[$functionName] = @()
@@ -81,12 +143,17 @@ function disposeFile {
 
 	write-Output "===== Parse 2 - Find Calling Functions ====="
 
-	# Reset counters
+	# Set variables
+	# This variable tracks how many depths of { we are.  When we return to 0 then we have finished a function
 	$countDepth = 0
+
+	# If we have found a function keyword, then the next token is the function name
 	$foundFunction = $false
+
+	# This is used to determine if the next token is a filename when dot sourced
 	$foundIncludeOperator = $false
 
-	# Iterate over finding called functions
+	# Iterate over all tokens
 	foreach ($token in $Tokens) {
 
 		# We are interested in the Type = function, then capture the command argument after it
@@ -96,6 +163,7 @@ function disposeFile {
 		}
 		# Record the name of the function
 		if ($foundFunction) {
+			# Add the name to the stack as we can have functions nested in functions.
 			$callStack.push( ($token.Content).ToLower())
 			$foundFunction = $false
 			write-Debug "Line $($token.StartLine) - Found function: '$($callstack.peek())'"
@@ -128,6 +196,7 @@ function disposeFile {
 	#		write-debug "Line $($token.StartLine) - GroupDepth is $countDepth"
 			if (!$countDepth -and $callstack.peek() -ne 'main') {
 				write-debug "Line $($token.StartLine) - Exiting function $($callstack.peek())"
+				# Pop the name off the stack, so we return to the previous function name
 				$callStack.pop() | Out-Null
 				write-debug "    Current function is now: $($callStack.peek())"
 			}
@@ -136,9 +205,12 @@ function disposeFile {
 		# If it is a command or CommandArgument, see if it is in the function tables, if not then register it
 		if ($token.Type -eq 'CommandArgument' -or $token.Type -eq 'Command') {
 			$calledFunction = ($token.Content).ToLower()
+			# If the function is in a list of functions that we know about...
 			if ($functions.ContainsKey($calledFunction)) {
+				# ... and we have not already put this function name into it
 				if ($functions[$callstack.peek()] -notcontains $calledFunction) {
 					write-debug "Line $($token.StartLine) - Adding $($callstack.peek()) calling $calledFunction"
+					# Add the called function to the function being processed
 					$functions[$callstack.peek()] += $calledFunction
 				}
 			}
@@ -151,8 +223,9 @@ foreach ($file in ($scriptFile.split(','))) {
 	disposeFile -file $file
 }
 
+# Get the name of the powershell file
 $filename = Split-Path $scriptFile -Leaf
-# Generate the DOT file content
+# Generate the Mermaid content
 $mermaidContent += "---`n"
 $mermaidContent += "title: PowerShell Script - $filename`n"
 $mermaidContent += "---`n"
